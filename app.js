d9a84dc7724ac45b480c293a3bae1ba2ccf1216d
WinterZz64 <kauaenger02@gmail.com>
2026-06-25 14:41:49 -0300

    Atualização da aplicação

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-function clicar() {
-    alert("Funcionou!");
-}
+console.log("Página carregada com sucesso");
+
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", () => {
+    console.log("Botão clicado");
+    alert("Você clicou no botão!");
+});
